fe654cb472675c37ccceb353616f500290ebc277
Amdounislim <amdouni.slim@gmail.com>
2020-10-07 10:59:43 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Counter from './components/Counter'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      // title: "WS-State 01",
+      // obj : {
+      //   name: "yahia", 
+      //   age: 25
+      // }
+      counter : 0,
+      myinput:""
+    }
+  }
+
+  increment=()=>{
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  }
+
+  decrement=()=>{
+    this.setState({
+      counter: this.state.counter - 1
+    })
+  }
+
+  handelChange=(e)=>this.setState({myinput:e.target.value})
+
+
+  render() {
+    return (
+      <div className="App">
+        {/* <h1>{this.state.title}</h1>
+    <h3>{this.state.obj.name}</h3> */}
+            <button onClick={this.increment}>+</button>
+            {/* <h4>{this.state.counter}</h4> */}
+            <Counter count={this.state.counter}/>
+            <button onClick={this.decrement}>-</button>
+            <br/>
+            <input type='text' onChange={this.handelChange} />
+            <br/>
+  <span>{this.state.myinput}</span>
+      </div>
+    )
+  }
 }
 
-export default App;
